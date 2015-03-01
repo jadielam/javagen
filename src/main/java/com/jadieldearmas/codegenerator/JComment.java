60b91e5846d474cf4baf1b389bc354e0b2a5b201
jadielam <jadielam@gmail.com>
2015-02-28 19:27:00 -0500

    Added comments for JComment.java

--- src/main/java/com/jadieldearmas/codegenerator/JComment.java
+++ src/main/java/com/jadieldearmas/codegenerator/JComment.java
@@ -9,13 +9,21 @@
 package com.jadieldearmas.codegenerator;
 
 /**
+ * This class is the Java model for a java comment. It is abstract because
+ * it will support two types of comments:
+ * -javadoc comment
+ * -regular comment
+ * 
+ * Every class that extends this class needs to call the constructor defined here.
  * @author jdearmas
  *
- * @since  
+ * @since 1.0
  */
 public abstract class JComment extends JavaTemplateGroup {
 
 	/**
+	 * Constructor that will be called by the classes
+	 * extending this class.
 	 * @param templateName
 	 */
 	JComment(String templateName) {
