6f29e45c5998f0d2222893ce47b207321cd59c47
jadielam <jadielam@gmail.com>
2015-02-28 22:37:20 -0500

    Added comments to JConstructor.java

--- src/main/java/com/jadieldearmas/codegenerator/JConstructor.java
+++ src/main/java/com/jadieldearmas/codegenerator/JConstructor.java
@@ -9,37 +9,52 @@
 package com.jadieldearmas.codegenerator;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.stringtemplate.v4.ST;
 
 
 
 /**
+ * Java model for a class constructor
  * @author jdearmas
  *
  * @since  
  */
 public class JConstructor extends JavaTemplateGroup {
 	
+	/**
+	 * True if the body of the constructor is already added.
+	 * False if not. TODO: Should be done away with.
+	 */
 	private boolean isBodyAdded = false;
 	
+	/**
+	 * The name of the constructor
+	 */
 	private final String constructorName;
 	
+	/**
+	 * The vissibility modifier of the constructor.
+	 * It can take the values: "public", "private" or <code>null</code>
+	 */
 	private String vissibilityModifier;
 	
+	/**
+	 * The text body of the constructor.
+	 * TODO: Do away with it, and simply add a Body template to this thing.
+	 */
 	private String textBody;
 	
+	/**
+	 * The function arguments of the constructor
+	 */
 	private List<Pair<String, String>> functionArguments = new ArrayList<Pair<String, String>>();
 	
 	
 	/**
-	 * 
-	 * @param className
-	 * @param vissibilityModifier
-	 * @param someAttributeValues
+	 * The constructor of the constructor model
+	 * @param className The name of the class to which this constructor belongs.
 	 */
 	JConstructor(String className){
 		
@@ -47,32 +62,58 @@ public class JConstructor extends JavaTemplateGroup {
 		this.constructorName = className;
 	}
 	
+	/**
+	 * Sets the constructor to be public.
+	 * @return Returns <code>this</code> constructor model.
+	 */
 	public JConstructor setPublic(){
 		this.vissibilityModifier = "public";
 		return this;
 	}
 	
+	/**
+	 * Sets the constructor to be private
+	 * @return Returns <code>this</code> constructor model.
+	 */
 	public JConstructor setPrivate(){
 		this.vissibilityModifier = "private";
 		return this;
 	}
 	
+	/**
+	 * Sets the constructor to be protected.
+	 * @return Returns <code>this</code> constructor model.
+	 */
 	public JConstructor setProtected(){
 		this.vissibilityModifier = "protected";
 		return this;
 	}
 	
+	/**
+	 * Sets the constructor to be package-protected.
+	 * @return Returns <code>this</code> constructor model.
+	 */
 	public JConstructor setPackageProtected(){
 		this.vissibilityModifier = null;
 		return this;
 	}
 	
+	/**
+	 * Adds an argument to the constructor.
+	 * @param argumentType The Java type of the argument
+	 * @param argumentName The name/identifier of the argument
+	 * @return Returns <code>this</code> constructor model.
+	 */
 	public JConstructor addArgument(String argumentType, String argumentName){
 		this.functionArguments.add(new Pair<String, String>(argumentType, argumentName));
 		return this;
 	}
 	
 	/**
+	 * This method adds a body to the constructor as text.
+	 * TODO: Remove the throwing of exception and just replace the previous body of the
+	 * constructor. I don't know. This one looks complex.
+	 * 
 	 * The body of the Constructor can be added only once.
 	 * @param body of the constructor
 	 * @throws UnsupportedOperationException thrown whenever the body
@@ -90,6 +131,11 @@ public class JConstructor extends JavaTemplateGroup {
 		}
 	}
 	
+	/**
+	 * Creates a new coded body model.
+	 * @return Returns the coded body model just created
+	 * @throws UnsupportedOperationException if a body already exists.
+	 */
 	public JCodedBody addCodedBody() throws UnsupportedOperationException{
 		if (!this.isBodyAdded){
 			JCodedBody body = new JCodedBody();
@@ -104,6 +150,11 @@ public class JConstructor extends JavaTemplateGroup {
 		}
 	}
 	
+	/**
+	 * Adds a new annotation to the constructor.
+	 * @param name The name of the annotation
+	 * @return Returns the annotation that was created. 
+	 */
 	public JAnnotation addAnnotation(String name){
 		
 		JAnnotation annotation = new JAnnotation(name);
@@ -111,19 +162,31 @@ public class JConstructor extends JavaTemplateGroup {
 		return annotation;
 	}
 
-	
+	/**
+	 * Adds a new comment to the constructor
+	 * @param comment The string of the comment.
+	 * @return The comment that was created
+	 * @throws UnsupportedOperationException whenever the method is called, because
+	 * this operation is not yet supported.
+	 */
 	public JComment addComment(String comment) throws UnsupportedOperationException{
 		//TODO
 		throw new UnsupportedOperationException("Operation not yet supported");
 	}
 	
+	/**
+	 * Adds a javadoc style comment to the constructor
+	 * @return Returns the comment that was created
+	 * @throws UnsupportedOperationException whenever the method is called, because
+	 * this operation is not yet supported.
+	 */
 	public JComment addJavadocComment() throws UnsupportedOperationException {
 		//TODO
 		throw new UnsupportedOperationException("Operation not yet supported");
 	}
 	
 	/**
-	 * 
+	 * @see JavaTemplateGroup#compile()
 	 */
 	@Override
 	public void compile(){
@@ -148,7 +211,6 @@ public class JConstructor extends JavaTemplateGroup {
 	}
 
 	/** 
-	 * (non-Javadoc)
 	 * @see java.lang.Object#hashCode()
 	 */
 	@Override
@@ -168,7 +230,7 @@ public class JConstructor extends JavaTemplateGroup {
 
 	/** 
 	 * Equals based on fields constructorName and attributePairs.
-	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
 	@Override
