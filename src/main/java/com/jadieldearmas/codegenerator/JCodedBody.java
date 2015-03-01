b06d53ffcde647749bef33de8f5b89791920bfd6
jadielam <jadielam@gmail.com>
2015-02-28 19:22:18 -0500

    Addded comments to JCodedBody

--- src/main/java/com/jadieldearmas/codegenerator/JCodedBody.java
+++ src/main/java/com/jadieldearmas/codegenerator/JCodedBody.java
@@ -9,7 +9,7 @@
 package com.jadieldearmas.codegenerator;
 
 /**
- * To be used for the body of functions that will be generated
+ * Model for Java body that will be used for the body of functions that will be generated
  * programmatically, instead of from a template in a file.
  * @author jdearmas
  *
@@ -17,11 +17,20 @@ package com.jadieldearmas.codegenerator;
  */
 public class JCodedBody extends JBody {
 
+	/**
+	 * Creates a new Coded Body model
+	 */
 	JCodedBody(){
 		super("body");
 		
 	}
 	
+	/**
+	 * Adds new code unit model to body
+	 * @return Returns the code unit model just created.
+	 * @throws UnsupportedOperationException whenever the method is called, because 
+	 * the functionality is not yet supported.
+	 */
 	public JCodeUnit addCodeUnit() throws UnsupportedOperationException {
 		//TODO
 		throw new UnsupportedOperationException("Operation not yet supported");
